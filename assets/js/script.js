bfea35387dd200b8201e3120adf4e6b1d66158e4
Pisen99 <patricia.andersson99@hotmail.com>
2023-01-02 13:42:16 +0000

    Feat: added another class in css. Made 2 functions in javascript to open/close rules on webpage

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* ------------------------------------------------------ Rules popup */
+const rulesPopUp = document.getElementById('rules-container');
+
+function popupContainer() {
+    rulesPopUp.style.visibility = 'visible';
+}
+
+function popupContainerClose() {
+    rulesPopUp.style.visibility = 'hidden';
+}
